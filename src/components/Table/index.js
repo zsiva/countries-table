a9d8b09b3de7b24a4b1051f898eb2ac687ee9104
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-12-16 14:36:51 +0100

    Add filtering for the countries on the table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Table from '@trendmicro/react-table';
+import { Input } from 'semantic-ui-react';
 import sortAttributes from '../../utils/sortAttributes';
 
 export default class CountryTable extends Component {
   state = {
     sortColumnKey: '',
     sortOrder: 'asc',
-    sortedItems: this.props.items.sort(sortAttributes('countryName', 'asc')),
+    sortedItems: this.props.items.sort(sortAttributes('name', 'asc')),
     filterString: '',
   };
 
@@ -20,27 +21,44 @@ export default class CountryTable extends Component {
     }));
   };
 
+  onChange = (event, { value }) => {
+    this.setState(state => ({
+      filterString: value,
+      sortedItems: this.props.items.filter(it =>
+        it.name.toLowerCase().includes(value.toLowerCase()),
+      ),
+    }));
+  };
+
   render() {
     const { sortColumnKey, sortOrder } = this.state;
-    const columns = Object.keys(this.state.sortedItems[0]).map(it => ({
+    const columns = Object.keys(this.props.items[0]).map(it => ({
       title: it.replace(/_/g, ' '),
       dataIndex: it,
     }));
 
     return (
-      <Table
-        className="ui celled striped table"
-        data={this.state.sortedItems}
-        onRowClick={(record, index, e) => {
-          this.props.onSelect(record.name);
-        }}
-        columns={columns.map(column => ({
-          ...column,
-          sortable: true,
-          onClick: this.toggleSortOrder(column),
-          sortOrder: column.dataIndex === sortColumnKey ? sortOrder : '',
-        }))}
-      />
+      <Fragment>
+        <Input
+          onChange={this.onChange}
+          icon={{ name: 'search', circular: true, link: true }}
+          placeholder="Search..."
+        />
+
+        <Table
+          className="ui celled striped table"
+          data={this.state.sortedItems}
+          onRowClick={(record, index, e) => {
+            this.props.onSelect(record.name);
+          }}
+          columns={columns.map(column => ({
+            ...column,
+            sortable: true,
+            onClick: this.toggleSortOrder(column),
+            sortOrder: column.dataIndex === sortColumnKey ? sortOrder : '',
+          }))}
+        />
+      </Fragment>
     );
   }
 }
