80d73222114310de1ef5af31ec7f54140e62dc5c
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-12-16 12:21:44 +0100

    Create project with react + redux
    Add reducers & actions to fetch the countries

---
+++ src/actions/index.js
@@ -0,0 +1,41 @@
+export const FETCH_ITEMS_BEGIN = 'FETCH_ITEMS_BEGIN';
+export const FETCH_ITEMS_SUCCESS = 'FETCH_ITEMS_SUCCESS';
+export const FETCH_ITEMS_FAILURE = 'FETCH_ITEMS_FAILURE';
+
+export const dataParameters = [
+  'bigmac_index',
+  'life_expectancy',
+  'happiness_index',
+  'median_age',
+  'population',
+];
+
+export function fetchData() {
+  return dispatch => {
+    dispatch(fetchItemsBegin());
+
+    fetch(
+      `http://inqstatsapi.inqubu.com/?api_key=8fe15e4caa7ef85c&data=${dataParameters.join(
+        ',',
+      )}&cmd=getWorldData`,
+    )
+      .then(response => response.json())
+      .then(items => {
+        dispatch(fetchItemsSuccess(items));
+      })
+      .catch(err => dispatch(fetchItemsFailure(err)));
+  };
+}
+export const fetchItemsBegin = () => ({
+  type: FETCH_ITEMS_BEGIN,
+});
+
+export const fetchItemsSuccess = items => ({
+  type: FETCH_ITEMS_SUCCESS,
+  payload: { items },
+});
+
+export const fetchItemsFailure = error => ({
+  type: FETCH_ITEMS_FAILURE,
+  payload: { error },
+});
---
+++ src/reducers/countries.js
@@ -0,0 +1,49 @@
+import {
+  FETCH_ITEMS_BEGIN,
+  FETCH_ITEMS_SUCCESS,
+  FETCH_ITEMS_FAILURE,
+  dataParameters,
+} from '../actions';
+
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+export default function countries(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_ITEMS_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case FETCH_ITEMS_SUCCESS:
+      const filteredItems = action.payload.items.map(item => ({
+        countryName: item.countryName,
+        bigmac_index: item.bigmac_index,
+        happiness_index: item.happiness_index,
+        median_age: item.median_age,
+        life_expectancy: item.life_expectancy,
+        population: item.population,
+      }));
+      return {
+        ...state,
+        loading: false,
+        items: filteredItems,
+      };
+
+    case FETCH_ITEMS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        items: [],
+      };
+
+    default:
+      return state;
+  }
+}
